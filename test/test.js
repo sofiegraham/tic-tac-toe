594233ad627ada2dafc394a80597516b61571081
sofiegraham <sofiegraham@hotmail.com>
2017-08-13 16:33:56 -0700

    Added validation tests for each empty position

--- test/test.js
+++ test/test.js
@@ -12,8 +12,68 @@ game takes input from player(or AI)
 
 describe('Board', function() {
   /*
+  board should be viewable in the terminal
+  should say if a move is valid(pos)
+  should make a move
+  should check for win
+
 
 
   */
+
+  let testBoard;
+  let positions = generatePositions(new Board());
+  let sigils = ["X", "O"];
+
+  function generatePositions(board) {
+    const positionIdxs = [];
+    board.grid.forEach((arr ,idxY) => {
+      arr.forEach((el, idxX) => {
+        positionIdxs.push([idxX,idxY]);
+      });
+    });
+    return positionIdxs;
+  }
+
+  beforeEach(() => {
+    testBoard = new Board;
+  });
+
+  describe('constructor()', () => {
+
+    it('is initialized to a 3 x 3 array', () => {
+      assert(testBoard.grid.length === 3);
+      assert(testBoard.grid[0].length === 3);
+      assert(testBoard.grid[1].length === 3);
+      assert(testBoard.grid[2].length === 3);
+    });
+
+    it('is empty at the start of the game', () => {
+      function checkEmpty(arr) {
+        return arr.every(function(el) {
+          return el === undefined;
+        })
+      }
+      assert(checkEmpty(testBoard.grid[0]) === true);
+      assert(checkEmpty(testBoard.grid[1]) === true);
+      assert(checkEmpty(testBoard.grid[2]) === true);
+    });
+  });
+
+  describe('isValidMove()', () => {
+
+    positions.forEach(function(pos) {
+      sigils.forEach(function(sigil) {
+        it(`returns true if the ${sigil} can be placed in ${pos}`, () => {
+          assert(testBoard.isValidMove(pos, sigil) === true);
+        });
+      });
+    });
+  });
+
+  it(`returns false if pos is full`, () => {
+    testBoard[0][0] = "O";
+    assert(testBoard.isValidMove(1, sigil) === true)
+  })
 
 });
