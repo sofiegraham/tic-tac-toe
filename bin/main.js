7a1162102e00a460b1f4d8df6068ade4870cc484
sofiegraham <sofiegraham@hotmail.com>
2017-08-13 15:16:58 -0700

    Added node test and play functionality

---
+++ bin/main.js
@@ -0,0 +1,4 @@
+const Game = require('../lib/game.js');
+
+const game = new Game();
+game.play();
---
+++ test/test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const Board = require('../lib/board.js');
+const Game = require('../lib/game.js');
+
+/*
+game takes input from player(or AI)
+
+
+*/
+
+
+describe('Board', function() {
+  /*
+
+
+  */
+  
+})
